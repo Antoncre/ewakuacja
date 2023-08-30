df687430deec7ff1d117edc3d17d2dd94d2bc25c
Antoncre <antonij_gujwan@protonmail.com>
2023-08-30 14:57:06 +0200

    Improved functionality

--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 import os
 import subprocess
 import sqlite3
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, NoTransition
+from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.graphics import Color, Line
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
+
 
 # pip install pyinstaller
-
-Window.clearcolor = (0, 0, 0, 1)
-Window.maximize()
 
 
 def pesel_checker(pesel):
@@ -74,7 +75,8 @@ def pesel_checker(pesel):
 
 
 class WindowManager(ScreenManager):
-    current_transition = NoTransition()
+    pass
+
 
 class MainWindow(Screen):
     conn = sqlite3.connect('my.db')
@@ -107,7 +109,6 @@ class MainWindow(Screen):
             print("File not found.")
 
     def typer(self):
-        self.print_btm.disabled = True
         self.surname.fine = False
         self.firstname.fine = False
         self.fathers_name.fine = False
@@ -142,7 +143,7 @@ class MainWindow(Screen):
                 self.firstname.fine = False
                 Color(1, 182/255, 20/255, 1)
                 Line(rectangle=(self.firstname.x, self.firstname.y,
-                                self.firstname.width, self.firstwname.height),
+                                self.firstname.width, self.firstname.height),
                      width=2)
                 
         with self.fathers_name.canvas.after:
@@ -221,9 +222,114 @@ class MainWindow(Screen):
                                 self.pesel.width, self.pesel.height),
                      width=2)
 
-        if self.surname.fine and self.name.fine and self.fathers_name.fine and \
-                self.pesel.fine and self.address.fine and self.number.fine:
+        with self.surname_1.canvas.after:
+            self.surname_1.canvas.after.clear()
+            if self.surname_1.text:
+                self.surname_1.fine = True
+            else:
+                self.surname_1.fine = False
+                Color(1, 182 / 255, 20 / 255, 1)
+                Line(rectangle=(self.surname_1.x, self.surname_1.y,
+                                self.surname_1.width, self.surname_1.height),
+                     width=2)
+
+        with self.name_1.canvas.after:
+            self.name_1.canvas.after.clear()
+            if self.name_1.text:
+                self.name_1.fine = True
+            else:
+                self.name_1.fine = False
+                self.name_1.fine = False
+                Color(1, 182 / 255, 20 / 255, 1)
+                Line(rectangle=(self.name_1.x, self.name_1.y,
+                                self.name_1.width, self.firstname.height),
+                     width=2)
+
+        with self.fathers_name_1.canvas.after:
+            self.fathers_name_1.canvas.after.clear()
+            if self.fathers_name_1.text:
+                self.fathers_name_1.fine = True
+            else:
+                self.fathers_name_1.fine = False
+                Color(1, 182 / 255, 20 / 255, 1)
+                Line(rectangle=(self.fathers_name_1.x, self.fathers_name_1.y,
+                                self.fathers_name_1.width, self.fathers_name_1.height),
+                     width=2)
+
+        with self.address_1.canvas.after:
+            self.address_1.canvas.after.clear()
+            if self.address_1.text:
+                self.address_1.fine = True
+            else:
+                self.address_1.fine = False
+                self.address_1.fine = False
+                Color(1, 182 / 255, 20 / 255, 1)
+                Line(rectangle=(self.address_1.x, self.address_1.y,
+                                self.address_1.width, self.address_1.height),
+                     width=2)
+
+        with self.number_1.canvas.after:
+            self.number_1.canvas.after.clear()
+            if self.number_1.text:
+                try:
+                    self.number_1.foreground_color = (0, 0, 0, 1)
+                    number_1 = int(self.number_1.text)
+                    self.number_1.background_color = 'white'
+                    if len(str(number_1)) < 9 or len(str(number_1)) > 12:
+                        self.number_1.fine = False
+                        Color(1, 182 / 255, 20 / 255, 1)
+                        Line(rectangle=(self.number_1.x, self.number_1.y,
+                                        self.number_1.width, self.number_1.height),
+                             width=2)
+                    else:
+                        self.number_1.fine = True
+                except ValueError:
+                    self.number_1.fine = False
+                    Color(1, 182 / 255, 20 / 255, 1)
+                    Line(rectangle=(self.number_1.x, self.number_1.y,
+                                    self.number_1.width, self.number_1.height),
+                         width=2)
+            else:
+                self.number_1.fine = False
+                Color(1, 182 / 255, 20 / 255, 1)
+                Line(rectangle=(self.number_1.x, self.number_1.y,
+                                self.number_1.width, self.number_1.height),
+                     width=2)
+
+        with self.pesel_1.canvas.after:
+            self.pesel_1.canvas.after.clear()
+            if self.pesel_1.text:
+                try:
+                    int(self.pesel_1.text)
+                    if pesel_checker(self.pesel_1.text):
+                        self.pesel_1.fine = True
+                    else:
+                        self.pesel_1.fine = False
+                        Color(1, 182 / 255, 20 / 255, 1)
+                        Line(rectangle=(self.pesel_1.x, self.pesel_1.y,
+                                        self.pesel_1.width, self.pesel_1.height),
+                             width=2)
+                except ValueError:
+                    self.pesel_1.fine = False
+                    Color(1, 182 / 255, 20 / 255, 1)
+                    Line(rectangle=(self.pesel_1.x, self.pesel_1.y,
+                                    self.pesel_1.width, self.pesel_1.height),
+                         width=2)
+            else:
+                Color(1, 182 / 255, 20 / 255, 1)
+                Line(rectangle=(self.pesel_1.x, self.pesel_1.y,
+                                self.pesel_1.width, self.pesel_1.height),
+                     width=2)
+
+        if self.surname.fine and self.firstname.fine and self.fathers_name.fine and \
+                self.pesel.fine and self.address.fine and self.number.fine and \
+                self.surname_1.fine and self.name_1.fine and self.fathers_name_1.fine and \
+                self.pesel_1.fine and self.address_1.fine and self.number_1.fine:
             self.print_btn.disabled = False
+            self.save_btn.disabled = False
+        else:
+            self.print_btn.disabled = True
+            self.save_btn.disabled = True
 
     def _on_keyboard_down(self, keycode):
         if self.surname.focus and keycode == 40:  # 40 - Enter key pressed
@@ -232,12 +338,20 @@ class MainWindow(Screen):
     def save_and_clear(self):
         conn = sqlite3.connect('my.db')
         c = conn.cursor()
-        insert_query = f"INSERT INTO osoby VALUES ('{self.surname.text}','{self.name.text}'," \
+        insert_query = f"INSERT INTO osoby VALUES ('{self.surname.text}','{self.firstname.text}'," \
                        f"'{self.fathers_name.text}', '{self.pesel.text}', '{self.address.text}', " \
                        f"'{self.number.text}')"
+        insert_query_2 = f"INSERT INTO osoby VALUES ('{self.surname_1.text}','{self.name_1.text}'," \
+                         f"'{self.fathers_name_1.text}', '{self.pesel_1.text}', '{self.address_1.text}', " \
+                         f"'{self.number_1.text}')"
         c.execute(insert_query)
+        c.execute(insert_query_2)
         conn.commit()
         conn.close()
+        self.print_btn.disabled = self.save_btn.disabled = True
+        self.surname.text = self.firstname.text = self.fathers_name.text = self.pesel.text = self.address.text = \
+            self.number.text = self.surname_1.text = self.name_1.text = self.fathers_name_1.text = self.number.text = \
+            self.pesel_1.text = self.address_1.text = self.number_1.text = ''
 
     def show_people(self):
         conn = sqlite3.connect('my.db')
@@ -259,9 +373,12 @@ class TableWindow(Screen):
 
 kv = Builder.load_file('my.kv')
 
+class EwakuacjaApp(MDApp):
 
-class EwakuacjaApp(App):
     def build(self):
+        Window.clearcolor = (0, 0, 0, 1)
+        Window.maximize()
+        self.theme_cls.theme_style = 'Dark'
         return kv
 
 
